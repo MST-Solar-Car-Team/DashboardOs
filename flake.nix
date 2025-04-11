d11a1483386de2aaead2f1a6c87f3ab508cd7089
kingoftheflame <matthew.l.mcclure186@gmail.com>
2025-04-11 00:06:55 -0500

    finally no errors!!!

--- flake.nix
+++ flake.nix
@@ -1,34 +1,73 @@
 {
-  description = "Build image";
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
-  outputs = { self, nixpkgs }: rec {
-    nixosConfigurations.rpi2 = nixpkgs.lib.nixosSystem {
-      modules = [
-        "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-raspberrypi.nix"
-        # "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
-        {
-          nixpkgs.config.allowUnsupportedSystem = true;
-          # nixpkgs.crossSystem = { system = "armv7l-linux"; };
-          nixpkgs.hostPlatform.system = "armv7l-linux";
-          nixpkgs.buildPlatform.system = "x86_64-linux"; #If you build on x86 other wise changes this.
-          # ... extra configs as above
-          
-          system.stateVersion = "24.05";
-          
-          #Pi Stuff
-          # boot.loader.grub.enable = false;
-          # boot.loader.generic-extlinux-compatible.enable = true;
+  description = "Raspberry Pi 3B Kiosk SD Image using cross-compilation";
 
-          # nixpkgs.crossSystem.system = "armv7l-linux";
-          # imports = [
-          #   <nixpkgs/nixos/modules/installer/sd-card/sd-image-aarch64.nix>
-          # ];
-
-        }
-      ];
-    };
-    images.rpi2 = nixosConfigurations.rpi2.config.system.build.sdImage;
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
   };
-}
 
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        # Architecture we're targeting
+        targetSystem = "aarch64-linux";
+
+        # For host tools
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+
+        # Correct way to create nixosSystem using lib
+        nixosSystem = nixpkgs.lib.nixosSystem {
+          system = targetSystem;
+          modules = [
+            "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
+
+            ({ config, pkgs, ... }: {
+              system.stateVersion = "24.05";
+
+              users.users.root.initialPassword = "raspberry";
+
+              networking.useDHCP = true;
+              services.openssh.enable = true;
+
+              users.users.pi = {
+                isNormalUser = true;
+                password = "raspberry";
+                extraGroups = [ "wheel" ];
+              };
+
+              # services.xserver.enable = true;
+              # services.xserver.windowManager.matchbox.enable = true;
+              # services.xserver.displayManager.auto.enable = true;
+              # services.xserver.displayManager.auto.user = "pi";
+
+              # services.xserver.displayManager.sessionCommands = ''
+              #   ${pkgs.xorg.xset}/bin/xset s off
+              #   ${pkgs.xorg.xset}/bin/xset -dpms
+              #   ${pkgs.xorg.xset}/bin/xset s noblank
+              #   ${pkgs.chromium}/bin/chromium --noerrdialogs --kiosk https://example.com --disable-translate --no-first-run --fast --fast-start --disable-infobars --disable-features=TranslateUI &
+              # '';
+
+              # environment.systemPackages = with pkgs; [
+              #   chromium
+              #   xorg.xset
+              # ];
+
+              boot.loader.grub.enable = false;
+              boot.loader.generic-extlinux-compatible.enable = true;
+
+              boot.initrd.availableKernelModules = [ "bcm2835_dma" "sdhci_iproc" "sdhci" "mmc_block" ];
+
+          
+              # boot.kernelPackages = pkgs.linuxPackages_rpi3;
+              hardware.enableRedistributableFirmware = true;
+            })
+          ];
+        };
+      in {
+        packages.rpiImage = nixosSystem.config.system.build.sdImage;
+        defaultPackage = self.packages.${system}.rpiImage;
+      });
+}
 
