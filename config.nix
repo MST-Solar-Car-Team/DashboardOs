c214f94ad656449182d58a6f43b45b995aed9ab1
kingoftheflame <matthew.l.mcclure186@gmail.com>
2025-04-24 00:54:48 -0500

    idk probably working kiosk display

--- config.nix
+++ config.nix
@@ -18,25 +18,37 @@
       extraGroups = [ "wheel" "networkmanager"];
     };
 
-    # services.xserver.enable = true;
-    # services.xserver.windowManager.matchbox.enable = true;
-    # services.xserver.displayManager.auto.enable = true;
-    # services.xserver.displayManager.auto.user = "pi";
+     # Auto loging
+    services.displayManager.autoLogin.user = "pi";
+    services.displayManager.autoLogin.enable = true;
 
-    # services.xserver.displayManager.sessionCommands = ''
-    #   ${pkgs.xorg.xset}/bin/xset s off
-    #   ${pkgs.xorg.xset}/bin/xset -dpms
-    #   ${pkgs.xorg.xset}/bin/xset s noblank
-    #   ${pkgs.chromium}/bin/chromium --noerrdialogs --kiosk https://example.com --disable-translate --no-first-run --fast --fast-start --disable-infobars --disable-features=TranslateUI &
-    # '';
+    services.cage = {
+      enable = true;
+      program = "${pkgs.firefox}/bin/firefox";
+      user = "pi";
+    };
 
-    # environment.systemPackages = with pkgs; [
-    #   chromium
-    #   xorg.xset
-    # ];
+    # wait for network and DNS
+    systemd.services."cage-tty1".after = [
+      "network-online.target"
+      "systemd-resolved.service"
+    ];
+
+
+    environment.systemPackages = with pkgs; [
+      chromium
+      xorg.xset
+    ];
 
     boot.loader.grub.enable = false;
     boot.loader.generic-extlinux-compatible.enable = true;
+
+    #allows for serial communication
+    boot.kernelParams = [
+      "console=ttyS1,115200n8"
+    ];
+
+    boot.initrd.kernelModules = [ "vc4" "bcm2835_dma" "i2c_bcm2835" ];
 
     boot.initrd.availableKernelModules = [ "bcm2835_dma" "sdhci_iproc" "sdhci" "mmc_block" ];
 
