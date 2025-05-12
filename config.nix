23474fd41c82eb7f33c88ebcf9a39235889809ec
kingoftheflame <matthew.l.mcclure186@gmail.com>
2025-05-12 18:45:34 -0500

    less screwed

--- config.nix
+++ config.nix
@@ -1,11 +1,11 @@
   { config, pkgs, dashboard, ... }: {
     system.stateVersion = "24.05";
 
-    users.users.root.initialPassword = "raspberry";
+    users.users.root.initialPassword = "pi";
 
     #networking
     networking.hostName = "pi";
-    networking.useDHCP = true;
+    # networking.useDHCP = true;
     networking.wireless.enable = true;
 
     networking.wireless.networks = {
@@ -28,33 +28,35 @@
 
     users.users.pi = {
       isNormalUser = true;
-      password = "raspberry";
-      extraGroups = [ "wheel" "networkmanager"];
+      password = "pi";
+      extraGroups = [ "wheel" "networkmanager" "video" "input"];
     };
 
      # Auto loging
-    # services.displayManager.autoLogin.user = "pi";
-    # services.displayManager.autoLogin.enable = true;
+    services.displayManager.autoLogin.user = "pi";
+    services.displayManager.autoLogin.enable = true;
 
-    # services.cage = {
-    #   enable = true;
-    #   # program = "${pkgs.firefox}/bin/firefox";
-    #   program = "${dashboard}/bin/dashboard";
-    #   user = "pi";
-    # };
 
-    # # wait for network and DNS
-    # systemd.services."cage-tty1".after = [
-    #   # "network-online.target"
-    #   "systemd-resolved.service"
-    # ];
+    services.cage = {
+      enable = true;
+      # program = "${pkgs.firefox}/bin/firefox";
+      program = "${dashboard.packages.x86_64-linux.default}/bin/dashboard";
+      user = "pi";  
+    };
+
+    
+
+    # wait for network and DNS
+    systemd.services."cage-tty1".after = [
+      # "network-online.target"
+      "systemd-resolved.service"
+    ];
 
 
     environment.systemPackages = [
-      # chromium
-      pkgs.xorg.xset
       dashboard.packages.x86_64-linux.default
       pkgs.xterm
+      pkgs.cage
     ];
 
     boot.loader.grub.enable = false;
